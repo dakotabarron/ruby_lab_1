3907ac640be69b3ea1ff2cd2370d9376868203ad
Dakota Barron <dakotabarron52@gmail.com>
2017-02-28 11:16:30 -0600

    started getNumber method

---
+++ hailstone.rb
@@ -0,0 +1,10 @@
+def getNumber
+	print "Please enter a positive integer for which to perform the hailstone sequence: "
+	userInput = gets.chomp
+
+	while userInput !=~ /^[0-9]+$/
+		print "Please enter a valid positive integer: "
+		userInput = gets.chomp
+
+	return userInput.to_i
+end
